1ebb9d40751fb040e0a79219252fc25fe490af7f
Ubuntu <ubuntu@ip-172-31-38-220.us-east-2.compute.internal>
2017-08-18 23:12:36 +0000

    first

---
+++ install_spark.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install default-jre default-jdk scala --assume-yes
+mkdir ~/Downloads
+wget https://archive.apache.org/dist/spark/spark-1.4.1/spark-1.4.1.tgz -P ~/Downloads  
+tar zxvf ~/Downloads/spark-* -C /usr/local  
+mv /usr/local/spark-* /usr/local/spark  
+SPARK_HOME="/usr/local/spark"
+PATH=$PATH:$SPARK_HOME/bin
+echo "export SPARK_HOME=/usr/local/spark\nexport PATH=$PATH:$SPARK_HOME/bin"
+chown -R ubuntu $SPARK_HOME 
+cp ./spark-env.sh $SPARK_HOME/conf/spark-env.sh  
+
